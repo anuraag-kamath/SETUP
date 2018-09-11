64539f04a7aa8a44e8173623f924d8b99d4afd4e
anuraag-kamath <anuraag.kamath@gmail.com>
2018-09-11 10:51:11 +0530

    Create pm2-start.sh

---
+++ pm2-start.sh
@@ -0,0 +1,9 @@
+pm2 start spm/app.js --name spm
+
+pm2 start uam/app.js --name uam
+
+pm2 start dms/app.js --name dms
+
+pm2 start proxy/app.js --name proxy
+
+pm2 start objects/app.js --name objects
